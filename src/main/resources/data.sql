1971be71f5fba89273f6e74dae5dc80243e3144d
Ra Ko <you@example.com>
2023-09-13 00:17:44 +0200

    initialize database by predefined values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO 
+	CATEGORIES (category_name, category_description, category_designator) 
+VALUES
+  	('Drama', 'Dramen', 'novel'),
+	('Thriller', 'Thriller', 'novel'),
+	('Fantasy', 'Fantasy-Bücher', 'novel'),
+  	('Naturiwssenschaften', 'Bücher über Naturwissenschaften', 'nonfiction'),
+  	('Informationstechnologie', 'Bücher über Informationstechnologien', 'nonfiction'),
+  	('Mathematik', 'Bücher über Mathematik', 'nonfiction');
