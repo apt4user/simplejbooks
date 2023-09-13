64d85ed0f2d9ffc6e0bb5708fcee06e85b6fd353
Ra Ko <you@example.com>
2023-09-13 16:24:09 +0200

    add author's attributes and association

--- src/main/java/simplejbooks/simplejbooks/model/AuthorEntity.java
+++ src/main/java/simplejbooks/simplejbooks/model/AuthorEntity.java
@@ -1,23 +1,39 @@
 package simplejbooks.simplejbooks.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Table(name = "authors")
 @Entity
 public class AuthorEntity {
 
+	@ManyToMany
+	List<BookEntity> books;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-    private Long id;
-	
-    public Long getId() {
+	private Long id;
+
+	@Column(name = "author_name")
+	private String authorName;
+
+	public Long getId() {
 		return id;
 	}
-	
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
 }
