59f77192677bc3195e1c72e162e0ca539c5d71ad
Ra Ko <you@example.com>
2023-09-13 23:46:19 +0200

    add controller method in order to render related objects

--- src/main/java/simplejbooks/simplejbooks/controller/SubcategoriesController.java
+++ src/main/java/simplejbooks/simplejbooks/controller/SubcategoriesController.java
@@ -1,13 +1,28 @@
 package simplejbooks.simplejbooks.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import simplejbooks.simplejbooks.model.SubcategoryEntity;
 import simplejbooks.simplejbooks.service.SubcategoryService;
 
-@Repository
+@Controller
 public class SubcategoriesController {
 
 	@Autowired
 	SubcategoryService service;
-	
+
+	@RequestMapping(path = "/subcategories/{id}")
+	public String index(Model model, @PathVariable("id") Long id) {
+		List<SubcategoryEntity> subcategories = service.getAllRelatedSubcategories(id);
+		model.addAttribute("subcategories", subcategories);
+
+		return "/subcategories/index";
+	}
+
 }
--- src/main/java/simplejbooks/simplejbooks/service/SubcategoryService.java
+++ src/main/java/simplejbooks/simplejbooks/service/SubcategoryService.java
@@ -1,7 +1,12 @@
 package simplejbooks.simplejbooks.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import simplejbooks.simplejbooks.model.SubcategoryEntity;
 import simplejbooks.simplejbooks.repository.SubcategoryRepository;
 
 @Service
@@ -9,5 +14,23 @@ public class SubcategoryService {
 
 	@Autowired
 	SubcategoryRepository repository;
-	
+
+	public List<SubcategoryEntity> getAllRelatedSubcategories(Long id) {
+		
+		List<SubcategoryEntity> subcategoryEntities = (List<SubcategoryEntity>) repository.findAll();		
+		List<SubcategoryEntity> subcategoryEntity = new ArrayList<SubcategoryEntity>();
+		
+		for (SubcategoryEntity subcategory : subcategoryEntities) {
+			// Später durch HQL ersetzen
+			if (subcategory.getCategory().getId().equals(id)) {
+				subcategoryEntity.add(subcategory);
+			}
+		}
+
+		if (subcategoryEntity.size() > 0) {
+			return subcategoryEntity;
+		} else {
+			return new ArrayList<SubcategoryEntity>();
+		}
+	}
 }
