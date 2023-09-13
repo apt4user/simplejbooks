185a48400d8ae2c5973e5839ffd94905c0933e78
Ra Ko <you@example.com>
2023-09-14 00:26:31 +0200

    add controller method in order to render related objects

--- src/main/java/simplejbooks/simplejbooks/controller/BooksController.java
+++ src/main/java/simplejbooks/simplejbooks/controller/BooksController.java
@@ -1,8 +1,14 @@
 package simplejbooks.simplejbooks.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import simplejbooks.simplejbooks.model.BookEntity;
 import simplejbooks.simplejbooks.service.BookService;
 
 @Controller
@@ -11,12 +17,14 @@ public class BooksController {
 	@Autowired
 	BookService service;
 	
-	@RequestMapping("/books")
-	public String index() {
+	@RequestMapping(path = "/books/{id}")
+	public String index(Model model, @PathVariable("id") Long id) {
+		List<BookEntity> books = service.getAllRelatedBooks(id);
+		model.addAttribute("books", books);
 
 		return "/books/index";
 	}
-
+	
 	@RequestMapping("/books/new")
 	public String newBook() {
 
