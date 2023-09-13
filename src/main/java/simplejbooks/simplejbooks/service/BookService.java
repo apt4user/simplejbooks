2984daf2110b5faf7aea6518d2d0dbf4b14c825e
Ra Ko <you@example.com>
2023-09-14 00:29:04 +0200

    find related object by id in order to render books

--- src/main/java/simplejbooks/simplejbooks/service/BookService.java
+++ src/main/java/simplejbooks/simplejbooks/service/BookService.java
@@ -1,8 +1,12 @@
 package simplejbooks.simplejbooks.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import simplejbooks.simplejbooks.model.BookEntity;
 import simplejbooks.simplejbooks.repository.BookRepository;
 
 @Service
@@ -10,5 +14,24 @@ public class BookService {
 
 	@Autowired
 	BookRepository repository;
+	
+	public List<BookEntity> getAllRelatedBooks(Long id) {
+		
+		List<BookEntity> bookEntities = (List<BookEntity>) repository.findAll();		
+		List<BookEntity> bookEntity = new ArrayList<BookEntity>();
+		
+		for (BookEntity book : bookEntities) {
+			// Später durch HQL ersetzen
+			if (book.getSubcategory().getId().equals(id)) {
+				bookEntity.add(book);
+			}
+		}
+
+		if (bookEntity.size() > 0) {
+			return bookEntity;
+		} else {
+			return new ArrayList<BookEntity>();
+		}
+	}
 	
 }
