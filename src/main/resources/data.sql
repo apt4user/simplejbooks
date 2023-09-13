2f89d2db7a073e3a8dbf17e7bb5b7cc5a7b52d86
Ra Ko <you@example.com>
2023-09-13 13:51:25 +0200

    create table and initialize attributes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,14 @@ VALUES
   	('Naturiwssenschaften', 'Bücher über Naturwissenschaften', 'nonfiction'),
   	('Informationstechnologie', 'Bücher über Informationstechnologien', 'nonfiction'),
   	('Mathematik', 'Bücher über Mathematik', 'nonfiction');
+  	
+  	INSERT INTO 
+	SUBCATEGORIES (subcategory_topic, subcategory_description, category_id) 
+VALUES
+  	('High Fantasy', 'Bücher des Subgenres High Fantasy', 3),
+	('Physik', 'Lehr-/ Sachbücher über Physik', 4),
+	('Chemie', 'Lehr-/ Sachbücher über Chemie', 4),
+  	('Algorithmen', 'Lehr-/ Sachbücher über Algorithmen', 5),
+  	('Netzwerktechnik', 'Lehr-/ Sachbücher über Netzwerktechnik', 5),
+  	('Analysis', 'Lehr-/ Sachbücher über Mathematik über Analysis', 6),
+  	('Lineare Algebra', 'Lehr-/ Sachbücher über Lineare Algebra', 6);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,5 +4,27 @@ CREATE TABLE CATEGORIES (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   category_name VARCHAR(250) NOT NULL,
   category_description VARCHAR(250) NOT NULL,
-  category_designator VARCHAR(250) DEFAULT NULL
+  category_designator VARCHAR(250) NOT NULL
+);
+
+DROP TABLE IF EXISTS SUBCATEGORIES;
+
+CREATE TABLE SUBCATEGORIES (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    subcategory_topic VARCHAR(250) NOT NULL,
+    subcategory_description VARCHAR(250) NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES CATEGORIES(id)
+);
+
+DROP TABLE IF EXISTS BOOKS;
+
+CREATE TABLE BOOKS (
+    id INT AUTO_INCREMENT  PRIMARY KEY,
+    book_title VARCHAR(250) NOT NULL,
+    book_isbn VARCHAR(250) NOT NULL,
+    book_description VARCHAR(250) NOT NULL,
+    book_price NUMERIC(5,2) NOT NULL,
+    subcategory_id INT,
+    FOREIGN KEY (subcategory_id) REFERENCES SUBCATEGORIES(id)
 );
