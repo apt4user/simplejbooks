147339124eaafae947442c1399de53df9260f35d
Ra Ko <you@example.com>
2023-09-14 18:22:17 +0200

    adjust reference name related to the corresponding entity

--- src/main/java/simplejbooks/simplejbooks/repository/CartRepository.java
+++ src/main/java/simplejbooks/simplejbooks/repository/CartRepository.java
@@ -3,9 +3,9 @@ package simplejbooks.simplejbooks.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import simplejbooks.simplejbooks.model.AuthorEntity;
+import simplejbooks.simplejbooks.model.CartEntity;
 
 @Repository
-public interface CartRepository extends CrudRepository<AuthorEntity, Long> {
+public interface CartRepository extends CrudRepository<CartEntity, Long> {
 
 }
